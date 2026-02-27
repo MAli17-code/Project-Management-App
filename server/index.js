9cca9b49c515c0a3b3f3991c3d8709f67a506a52
MAli17-code <136349002+MAli17-code@users.noreply.github.com>
2026-02-27 14:49:21 +0500

    I have just modified the main index.js file due to some error occurence

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ import express from 'express'
 import cors from 'cors'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
-import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
+import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(ClerkExpressRequireAuth())
+app.use(ClerkExpressWithAuth())
 
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
